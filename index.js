46d7f0b5ab2cc9bde3375667ac6d8baaaa27b013
Omprakash Choudhary <om78900987@gmail.com>
2022-05-18 22:15:55 +0530

    updated

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-//var text=document.getElementsByClassName('screen');
-function clearScreen(){
-    document.getElementById("result").value="";
-}
-function display(str)
-{
-    document.getElementById("result").value+=str;
-    return str;
-}
-function solve(){
-    var ans=document.getElementById("result").value;
-    var result=eval(ans);
-    document.getElementById("result").value=result;
-    return result;
+var buttons=document.getElementsByClassName('button');
+var input=document.getElementById('result');
+var clear=document.getElementsByClassName('clear');
+var op1=0;
+var op2=null;
+var op=null;
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click",function(){
+        var value=this.value;
+        if(value>=0 && value<=9||value=='.'){
+            input.value+=value;
+        }else if(value=='+'|| value=='-'||value=='*'||value=='/'){
+            op1=parseFloat(input.value);
+            op=value;
+            input.value="";
+        }else if(value=='='){
+            op2=parseFloat(input.value);
+            var result=eval(op1+" "+op+" "+op2);
+            input.value=result;
+        }else if(value=='C'){
+            input.value="";
+            op1=0;
+            op=null;
+            op2=null;
+        }
+
+    });
 }
